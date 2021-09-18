46e81f2b6a1f08dca2ff14018f340c6dc148c045
Ali Mustakim <alim@forwardcomputers.com>
2021-09-18 13:52:33 -0400

    updates [skip ci]

--- sharemouse/tools/chocolateyInstall.ps1
+++ sharemouse/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = $env:ChocolateyPackageName
-$version = '3.8.21'
+$version = '5.0.49'
 
 $installedVersion = Get-ItemPropertyValue -path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\774f2290-3906-58eb-baae-35b0dc01c31f" -Name "DisplayVersion" -ErrorAction SilentlyContinue
 if ( $version -eq $installedVersion ) {
@@ -12,11 +12,9 @@ if ( $version -eq $installedVersion ) {
 $packageArgs = @{
    packageName      = $packageName
    fileType         = 'EXE'
-   url64bit         = 'https://github.com/standardnotes/desktop/releases/download/v3.8.21/standard-notes-3.8.21-win.exe'
-   checksum64       = 'e3b2aa6f1f181469a79f4681ec6db74bca84f8d85699cd5199a00a8dd22b4c59'
-   checksumType64   = 'sha256'
-   softwareName     = 'Standard Notes*'
-   silentArgs       = '/S'
+   url64bit         = 'https://www.sharemouse.com/ShareMouseSetup.exe'
+   softwareName     = 'ShareMouse'
+   silentArgs       = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
    validExitCodes   = @(0)
 }
 
