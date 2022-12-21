bc5c0e9e88d73cd3c52aa951c2112a2ac23e9ae0
Ali Mustakim <alim@forwardcomputers.com>
2022-12-21 10:14:29 +0000

    github actions update [skip ci]

--- sharemouse/tools/chocolateyInstall.ps1
+++ sharemouse/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = $env:ChocolateyPackageName
-$version = '5.0.49'
+$version = '6.0.50'
 
 $installedVersion = Get-ItemPropertyValue -path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\774f2290-3906-58eb-baae-35b0dc01c31f" -Name "DisplayVersion" -ErrorAction SilentlyContinue
 if ( $version -eq $installedVersion ) {
--- standardnotes/tools/chocolateyInstall.ps1
+++ standardnotes/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = $env:ChocolateyPackageName
-$version = '3.8.21'
+$version = '3.22.11'
 
 $installedVersion = Get-ItemPropertyValue -path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\774f2290-3906-58eb-baae-35b0dc01c31f" -Name "DisplayVersion" -ErrorAction SilentlyContinue
 if ( $version -eq $installedVersion ) {
@@ -12,8 +12,8 @@ if ( $version -eq $installedVersion ) {
 $packageArgs = @{
    packageName      = $packageName
    fileType         = 'EXE'
-   url64bit         = 'https://github.com/standardnotes/desktop/releases/download/v3.8.21/standard-notes-3.8.21-win.exe'
-   checksum64       = 'e3b2aa6f1f181469a79f4681ec6db74bca84f8d85699cd5199a00a8dd22b4c59'
+   url64bit         = 'https://github.com/standardnotes/desktop/releases/download/v3.22.11/standard-notes-3.22.11-win.exe'
+   checksum64       = 'fb3d2084a86b122f5cef330f1974ad33790d6fb3207d3262af7f78f0d51fbe3b'
    checksumType64   = 'sha256'
    softwareName     = 'Standard Notes*'
    silentArgs       = '/S'
