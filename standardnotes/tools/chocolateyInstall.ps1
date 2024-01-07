defcb29d4ca552603141d37f52c5f834a1704465
Ali Mustakim <alim@forwardcomputers.com>
2024-01-07 10:11:48 +0000

    github actions update [skip ci]

--- standardnotes/tools/chocolateyInstall.ps1
+++ standardnotes/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '3.183.39'
+$version = '3.183.43'
 
 $installedVersion = Get-ItemPropertyValue -path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\774f2290-3906-58eb-baae-35b0dc01c31f' -Name 'DisplayVersion' -ErrorAction SilentlyContinue
 if ( $version -eq $installedVersion ) {
@@ -11,8 +11,8 @@ if ( $version -eq $installedVersion ) {
 $packageArgs = @{
    packageName      = $env:ChocolateyPackageName
    fileType         = 'EXE'
-   url64bit         = 'https://github.com/standardnotes/app/releases/download/@standardnotes/desktop@3.183.39/standard-notes-3.183.39-win.exe'
-   checksum64       = '17e6112cb8e3f584d062dc0e921540722c0e03496e2f674cb12b5ac0c5cfaf1d'
+   url64bit         = 'https://github.com/standardnotes/app/releases/download/@standardnotes/desktop@3.183.43/standard-notes-3.183.43-win.exe'
+   checksum64       = '1755537386377eb9540e5dd420a08ebabc9251f682a71b099a99dd1d7870982d'
    checksumType64   = 'sha265'
    softwareName     = 'Standard Notes*'
    silentArgs       = '/S'
