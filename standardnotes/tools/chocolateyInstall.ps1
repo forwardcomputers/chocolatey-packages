bd28d7edf276ca55c6bae631c4bc0f39ad4ad7ff
Ali Mustakim <alim@forwardcomputers.com>
2024-04-10 15:59:59 -0400

    updates [skip ci]

--- standardnotes/tools/chocolateyInstall.ps1
+++ standardnotes/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '3.192.13'
+$version = '3.192.13.20240410'
 
 $installedVersion = Get-ItemPropertyValue -path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\774f2290-3906-58eb-baae-35b0dc01c31f' -Name 'DisplayVersion' -ErrorAction SilentlyContinue
 if ( $version -eq $installedVersion ) {
