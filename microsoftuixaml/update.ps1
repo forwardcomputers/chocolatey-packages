6a583ff2b2c8c3d9e03751232fb73d5749345bd1
Ali Mustakim <alim@forwardcomputers.com>
2024-04-10 15:22:59 -0400

    updates [skip ci]

---
+++ microsoftuixaml/update.ps1
@@ -0,0 +1,39 @@
+import-module au
+Add-Type -AssemblyName System.Web
+
+$_pkgName   = Split-Path -Leaf $PSScriptRoot
+$nuspecInfo = ( [xml] ( Get-Content ".\$_pkgName.nuspec" ) ).package.metadata
+$repoOwner  = ( $nuspecInfo.projectSourceUrl ).split( '/' )[ -2 ]
+$repoApp    = ( $nuspecInfo.projectSourceUrl ).split( '/' )[ -1 ]
+$repoUri    = [System.Web.HttpUtility]::UrlDecode( "https://api.github.com/repos/$repoOwner/$repoApp/releases/latest" )
+$licenseUrl = $nuspecInfo.licenseUrl
+
+function global:au_SearchReplace {
+    @{
+        ".\legal\VERIFICATION.txt"      = @{
+            "(?i)(^\s*64-Bit software\:).*"             = "`${1} $($Latest.URL64)"
+            "(?i)(\s*has been obtained from\s*)\<.*\>"  = "`${1}<$($LicenseUrl)>"
+        }
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^[$]version\s*=\s*)('.*')"            = "`${1}'$($Latest.Version)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"           = "`${1}'$($Latest.URL64)'"
+       }
+    }
+}
+
+function global:au_GetLatest {
+    $repoPage             = Invoke-RestMethod -Method GET -Uri $repoUri -UseBasicParsing
+    $releaseVersion       = ( $repoPage.tag_name ).split( '@' )[ -1 ]
+    $releaseUrl64         = [System.Web.HttpUtility]::UrlDecode( ( $repoPage.assets | Where-Object name -like '*PreinstallKit.zip' ).browser_download_url )
+
+   @{ 
+      Version        = $releaseVersion
+      URL64          = $releaseUrl64
+   }
+}
+
+function global:au_AfterUpdate($Package) {
+    Invoke-WebRequest $licenseUrl -OutFile '.\legal\LICENSE.txt'
+}
+
+update -ChecksumFor none
